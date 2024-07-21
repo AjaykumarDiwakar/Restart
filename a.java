0dc5c4756c5fca56b712c33497a727633b177945
AjayKumarDiwakar <kumarajay7160@gmail.com>
2024-07-21 11:00:12 +0530

    Initial commit

---
+++ a.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class a{
+    public static void main(String[] args) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        
+        map.put(1, 1);
+        map.containsKey(1);
+        map.get(2);
+        // Set<Integer> set=new HashSet<>();
+        
+
+    }
+}
